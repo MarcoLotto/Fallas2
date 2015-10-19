bc1d8d5d35396d1c055ad774cc4e2f8321fdae29
alemarra <alemarra@gmail.com>
2015-10-19 09:00:39 -0300

    Agregada creacion bi-direccional

--- Semantic network/Relation.js
+++ Semantic network/Relation.js
@@ -1,17 +1,30 @@
 
-function Relation(name, concept1, concept2){
+Relation = function (name, concept1, concept2){
+
   this.name = name;
   this.concept1 = concept1;
   this.concept2 = concept2;
   
   this.apply = function(concept){
-	return (this.concept1.equals(concept) || this.concept2.equals(concept));
+	 return (this.concept1.equals(concept) || this.concept2.equals(concept));
   }
   
   this.getDestinationConcept = function(originConcept){
-	if(this.concept1.equals(originConcept)){
-		return this.concept2;
-	}
-	return this.concept1;
+  	if(this.concept1.equals(originConcept)){
+  		return this.concept2;
+  	}
+  	return this.concept1;
   }
-}
+};
+
+
+Relation.asDependency = function (concept1, name, concept2) {
+    concept1.addRelation(name, concept2);
+  };
+
+Relation.asRelation = function (concept1, name, concept2) {
+    concept1.addRelation(name, concept2);
+    concept2.addRelation(name, concept1);
+  }
+
+
